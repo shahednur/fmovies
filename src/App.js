1548e0f8cb7b24be9edf7b9998ef4814842ae648
shahednur <shahednurctg@gmail.com>
2022-02-11 01:56:47 +0600

    api integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Preloader from "./components/Preloader";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Header from "./components/Header";
-import Slider from "./components/Slider";
-import Movie from "./components/Movie";
-import Trailer from "./components/Trailer";
-import News from "./components/News";
 import Footer from "./components/Footer";
 
-function App() {
+import Routing from './config/Routing';
+
+function App(){
   return (
-    <div className="App">
-      <Preloader />
-      <Login />
-      <Register />
-      <Header />
-      <Slider />
-      <Movie />
-      <Trailer />
-      <News />
-      <Footer />
-    </div>
+    <Router>
+        <>
+          <Preloader />
+          <Login />
+          <Register />
+          <Header/>
+          <Routing />
+          <Footer />
+        </>
+    </Router>
   );
 }
 
